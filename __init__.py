66f900f46c7d1b54499ef58a55c963a486845129
Theun Kohlbeck <t.kohlbeck@gmail.com>
2018-01-18 00:13:44 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -63,7 +63,7 @@ class TriviaSkill(MycroftSkill):
                 self.speak(str(i) + ".    " + a)
 	    #self.play( 'think.wav' )
 	time.sleep(3)
-	reponse = self.getinput()	
+	response = self.getinput()	
         LOGGER.debug("The response data is: {}".format(response))
         self.speak("Your choice is "+ response)        
         if correct_answer == allanswers[int(response)-1]:
